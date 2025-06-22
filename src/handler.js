f7331e123b3408e91f1e59fc2801b8930e004d09
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-22 22:43:09 +0530

    Initial commit

---
+++ src/handler.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const login = async(req,res)=>{
+    const store = []
+    const data = req.body
+    console.log("data is : ",data)
+    for(const user of data){
+        const name = user.name
+        const password = user.password
+        const email = user.email
+        const phone = user.phone
+        const address = user.address
+        console.log(name,password,email,phone,address)
+        store.pushstore.push({ name, password, email, phone, address });
+        console.log(store)
+    }
+    res.send(store)
+}
+module.exports = login;
