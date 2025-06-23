941b832f84d83612ef5fa993bbce82e340360960
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-23 20:45:26 +0530

    register page

--- src/handler.js
+++ src/handler.js
@@ -7,13 +7,32 @@ const login = async(req,res)=>{
     for(const user of data){
         const name = user.name
         const password = user.password
-        const email = user.email
+        const email = user.email 
         const phone = user.phone
-        const address = user.address
-        console.log(name,password,email,phone,address)
+        console.log(name,password,email,phone)
         store.push({ name, password, email, phone, address });
         console.log(store)
     }
     res.send(store)
 }
 module.exports = login;
+
+const register = async(req,res)=>{
+    const reg = []
+    console.log("first time reg is : ",reg)
+    const data = req.body
+    console.log("data is : ",data)
+    for(const user of data){
+        const name = user.name
+        const password = user.password
+        const email = user.email 
+        const phone = user.phone
+        const address = user.address
+        const dob = user.dateofbirth
+    console.log(name,password,email,phone,address)
+    reg.push({ name, password, email, phone, address, dob });
+    console.log(reg)
+    }
+    res.send(reg)
+}
+module.exports = register;
