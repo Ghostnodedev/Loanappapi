63c87eccb0b8866dd2c2f4d92b43f327e6d496ad
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-06-23 20:57:42 +0530

    change validarion

--- src/handler.js
+++ src/handler.js
@@ -1,50 +1,50 @@
-"use strict"
+"use strict";
 
-const login = async(req,res)=>{
-    const store = []
-    const data = req.body
-    console.log("data is : ",data)
-    for(const user of data){
-        const name = user.name
-        const password = user.password
-        const email = user.email 
-        const phone = user.phone
-        if(!name || !password||!email ||!phone){
-            return{
-                status: false,
-                message: "Please fill all the fields"
-            }
-        }
-        console.log(name,password,email,phone)
-        store.push({ name, password, email, phone, address });
-        console.log(store)
+const login = async (req, res) => {
+  const store = [];
+  const data = req.body;
+  console.log("data is : ", data);
+  for (const user of data) {
+    const name = user.name;
+    const password = user.password;
+    const email = user.email;
+    const phone = user.phone;
+    if (!name || !password || !email || !phone) {
+      return {
+        status: false,
+        message: "Please fill all the fields",
+      };
     }
-    res.send(store)
-}
+    console.log(name, password, email, phone);
+    store.push({ name, password, email, phone, address });
+    console.log(store);
+  }
+  res.send(store);
+};
 module.exports = login;
 
-const register = async(req,res)=>{
-    const reg = []
-    console.log("first time reg is : ",reg)
-    const data = req.body
-    console.log("data is : ",data)
-    for(const user of data){
-        const name = user.name
-        const password = user.password
-        const email = user.email 
-        const phone = user.phone
-        const address = user.address
-        const dob = user.dateofbirth
-        if(!name || !password||!email ||!phone || !address || !dob){
-            return{
-                status: false,
-                message: "Please fill all the fields"
-            }
-        }
-    console.log(name,password,email,phone,address)
-    reg.push({ name, password, email, phone, address, dob });
-    console.log(reg)
+const register = async (req, res) => {
+  const reg = [];
+  console.log("first time reg is : ", reg);
+  const data = req.body;
+  console.log("data is : ", data);
+  for (const user of data) {
+    const name = user.name;
+    const password = user.password;
+    const email = user.email;
+    const phone = user.phone;
+    const address = user.address;
+    const dob = user.dateofbirth;
+    console.log(name, password, email, phone, address);
+    if (!name || !password || !email || !phone || !address || !dob) {
+      return {
+        status: false,
+        message: "Please fill all the fields",
+      };
     }
-    res.send(reg)
-}
+    reg.push({ name, password, email, phone, address, dob });
+    console.log(reg);
+  }
+  res.send(reg);
+};
 module.exports = register;
